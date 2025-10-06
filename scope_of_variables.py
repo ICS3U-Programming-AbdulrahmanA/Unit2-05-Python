8841eb7631f121a6d26250c27cb162b1bc3fc190
Abdulrahman Albinali <abdulrahman.albinali@stu.ocsb.ca>
2025-10-06 17:57:45 +0000

    debugger

--- scope_of_variables.py
+++ scope_of_variables.py
@@ -2,27 +2,69 @@
 # Created By: Abdul
 # Date: 10/5/2025
 # scope of variables
+#!/usr/bin/env python3
 
-# Global variable
-variable_X = 10
+# Created by: Ms Raffin
 
+# Created on: Sept 2019
 
-def local_variable():
-    variable_Y = 5
-    print("Inside local_variable() -> variable_Y:", variable_Y)
-    print("Accessing global variable_X from inside function:", variable_X)
+# This program shows how local and global variables work
+
+# global variable
+
+variable_X = 25
 
 
 def global_variable():
+
+    # this shows what happens with global variables
+
+    global variable_X
+
+    variable_X = variable_X + 1
+
+    global variable_Y
+    
+    variable_Y = 30
+    
     global variable_Z
-    variable_Z = 15
-    print("Inside global_variable() -> variable_Z:", variable_Z)
+    
+    variable_Z = variable_X + variable_Y
+
+    print(
+        "Global variable_X, variable_Y, variable_Z: {0} + {1} = {2}".format(
+            variable_X, variable_Y, variable_Z
+        )
+    )
+
+    
+def local_variable():
+
+    # this shows what happens with local variables
+
+    variable_X = 10
+
+    variable_Y = 30
+
+    variable_Z = variable_X + variable_Y
+
+    print(
+        "Local variable_X, variable_Y, variable_Z: {0} + {1} = {2}".format(
+            variable_X, variable_Y, variable_Z
+        )
+    )
 
 
-# Function calls
-if __name__ == "__main__":
+
+def main():
+
+    # this function shows how local and global variables work
+
     local_variable()
+
     global_variable()
 
-print("Outside functions -> variable_X:", variable_X)
-print("Outside functions -> variable_Z:", variable_Z)
+
+if __name__ == "__main__":
+
+    main()
